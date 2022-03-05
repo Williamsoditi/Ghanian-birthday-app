f291cd0f667257d6a8d1c7c44b1ead8263627004
Williamsoditi <williams.oditi@student.moringaschool.com>
2022-03-05 22:18:14 +0300

    Miscellaneous commit

--- js/script.js
+++ js/script.js
@@ -1,57 +1,57 @@
-// var form = document.getElementById('form')
-// // Form Validation to enter correct date format //
-// validDate = (birthdate) => {
-//     if (!birthdate || birthdate.split('-').length !== 3){
-//         alert("Enter Correct Date!");
-//         return false;
-//     }
-//     return true;
-// }
-// form.addEventListener('submit', function (e){
-//     e.preventDefault();
+var form = document.getElementById('form')
+// Form Validation to enter correct date format //
+validDate = (birthdate) => {
+    if (!birthdate || birthdate.split('-').length !== 3){
+        alert("Enter Correct Date!");
+        return false;
+    }
+    return true;
+}
+form.addEventListener('submit', function (e){
+    e.preventDefault();
 
-//     let birthdate = form.birthdate.value;
-//     let gender = form.gender.value;
+    let birthdate = form.birthdate.value;
+    let gender = form.gender.value;
 
-//     // form validation
-//     if (validDate(birthdate)) {
+    // form validation
+    if (validDate(birthdate)) {
         
-//         var splitted_date = birthday.split('-')
+        var splitted_date = birthday.split('-')
 
-//         var akanName = {
-//             female:{
-//                 sunday:"Akosua",
-//                 monday:"Adwoa",
-//                 tuesday:"Abenaa",
-//                 wednesday:"Akua",
-//                 thursday:"Yaa",
-//                 friday:"Afua",
-//                 saturday:"Ama",
-//             },
-//             male:{
-//                 sunday:"Kwasi",
-//                 monday:"Kwadwo",
-//                 tuesday:"Kwabena",
-//                 wednesday:"Kwaku",
-//                 thursday:"Yaw",
-//                 friday:"Kofi",
-//                 saturday:"Kwame",
-//             }
-//         }
+        var akanName = {
+            female:{
+                sunday:"Akosua",
+                monday:"Adwoa",
+                tuesday:"Abenaa",
+                wednesday:"Akua",
+                thursday:"Yaa",
+                friday:"Afua",
+                saturday:"Ama",
+            },
+            male:{
+                sunday:"Kwasi",
+                monday:"Kwadwo",
+                tuesday:"Kwabena",
+                wednesday:"Kwaku",
+                thursday:"Yaw",
+                friday:"Kofi",
+                saturday:"Kwame",
+            }
+        }
 
-//         var weekdays = [ "monday","tuesday","wednesday","thursday","friday","saturday","sunday"];
+        var weekdays = [ "monday","tuesday","wednesday","thursday","friday","saturday","sunday"];
 
-//         let valid_date = new Date(splitted_date[0],splitted_date[1],splitted_date[2]);
-//         let weekday_index = valid_date.getDay();
-//         let weekday_name = weekdays[weekday_index];
+        let valid_date = new Date(splitted_date[0],splitted_date[1],splitted_date[2]);
+        let weekday_index = valid_date.getDay();
+        let weekday_name = weekdays[weekday_index];
 
-//         let possible_names = akan_names[gender];
-//         let akanName = possible_names[weekday_name];
+        let possible_names = akan_names[gender];
+        let akanName = possible_names[weekday_name];
 
-//         var yourName = document.getElementById('myAkan');
-//         yourName.innerHTML = akanName;
-//     } else {
-//         return false;
-//     }
-// })
+        var yourName = document.getElementById('myAkan');
+        yourName.innerHTML = akanName;
+    } else {
+        return false;
+    }
+})
 
