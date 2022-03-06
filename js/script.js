bfea62dac25ebfc7065f0d6c2f545434a797a621
Williamsoditi <williams.oditi@student.moringaschool.com>
2022-03-06 07:57:33 +0300

    Chore : Resolved error - This resolves the javascript error

--- js/script.js
+++ js/script.js
@@ -1,57 +1,63 @@
-var form = document.getElementById('form')
-// Form Validation to enter correct date format //
-validDate = (birthdate) => {
-    if (!birthdate || birthdate.split('-').length !== 3){
-        alert("Enter Correct Date!");
-        return false;
-    }
-    return true;
+let form  = document.getElementById('myAkan')
+
+validDate= (birthday)=>{
+  if(!birthday || birthday.split('-').length !== 3){
+    alert("Please enter a valid date format!")
+    return false
+  }
+  return true
 }
+
+
 form.addEventListener('submit', function (e){
-    e.preventDefault();
+  e.preventDefault()
 
-    let birthdate = form.birthdate.value;
-    let gender = form.gender.value;
+  //Get form values
+  let birthday = form.birthday.value
 
-    // form validation
-    if (validDate(birthdate)) {
-        
-        var splitted_date = birthday.split('-')
+  let gender = form.gender.value
 
-        var akanName = {
-            female:{
-                sunday:"Akosua",
-                monday:"Adwoa",
-                tuesday:"Abenaa",
-                wednesday:"Akua",
-                thursday:"Yaa",
-                friday:"Afua",
-                saturday:"Ama",
-            },
-            male:{
-                sunday:"Kwasi",
-                monday:"Kwadwo",
-                tuesday:"Kwabena",
-                wednesday:"Kwaku",
-                thursday:"Yaw",
-                friday:"Kofi",
-                saturday:"Kwame",
-            }
-        }
 
-        var weekdays = [ "monday","tuesday","wednesday","thursday","friday","saturday","sunday"];
+  if(validDate(birthday)){
 
-        let valid_date = new Date(splitted_date[0],splitted_date[1],splitted_date[2]);
-        let weekday_index = valid_date.getDay();
-        let weekday_name = weekdays[weekday_index];
+    let splitted_date = birthday.split('-') // ["YYYY","MM","DD"]
 
-        let possible_names = akan_names[gender];
-        let akanName = possible_names[weekday_name];
-
-        var yourName = document.getElementById('myAkan');
-        yourName.innerHTML = akanName;
-    } else {
-        return false;
+    let akan_names = {
+      male:{
+        sunday:"Kwasi",
+        monday:"Kwadwo",
+        tuesday:"Kwabena",
+        wednesday:"Kwaku",
+        thursday:"Yaw",
+        friday:"Kofi",
+        saturday:"Kwame",
+      },
+      female:{
+        sunday:"Akosua",
+        monday:"Adwoa",
+        tuesday:"Abenaa",
+        wednesday:"Akua",
+        thursday:"Yaa",
+        friday:"Afua",
+        saturday:"Ama",
+      }
     }
+
+    let weekdays = ["thursday","friday","saturday","sunday","monday","tuesday","wednesday"]
+
+    let valid_date = new Date(splitted_date[0],splitted_date[1],splitted_date[2])
+    let weekday_index = valid_date.getDay()
+    let weekday_name = weekdays[weekday_index]
+
+    let possible_names = akan_names[gender]
+    let akan_name = possible_names[weekday_name]
+
+    let elem = document.getElementById('myAkan')
+    elem.innerHTML = akan_name
+  }else{
+    return false
+  }
+
 })
+
 
